abfb7594f5489b5e0aefdd7275ba7a3998e96ca9
dylrlas <77942477+dylrlas@users.noreply.github.com>
2021-04-23 11:30:57 -0600

    Update WebServer.java

--- SimpleWebServer/src/edu/nmsu/cs/webserver/WebServer.java
+++ SimpleWebServer/src/edu/nmsu/cs/webserver/WebServer.java
@@ -93,7 +93,7 @@ private boolean stop()
  **/
 public static void main(String args[])
 {
-	int port = 8085;
+	int port = 8080;
 	if (args.length > 1)
 	{
 		System.err.println("Usage: java Webserver <portNumber>");
